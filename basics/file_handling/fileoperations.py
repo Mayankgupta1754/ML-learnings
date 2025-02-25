126cc3b24bee97fa5419db95a4cf85bb90ce23cf
Mayankgupta1754 <160992625+Mayankgupta1754@users.noreply.github.com>
2025-02-25 22:47:01 +0530

    Add files via upload

---
+++ basics/file_handling/fileoperations.py
@@ -0,0 +1,8 @@
+import os
+file = open(r"C:\Users\mayan\Desktop\ml\basics\file handling\textfile.txt", 'r')
+# print(file.read())
+
+# for line in file:
+#     print(file.readlines())
+file = open(r"C:\Users\mayan\Desktop\ml\basics\file handling\textfile2.txt", 'w')
+file.write("heyo file2")
